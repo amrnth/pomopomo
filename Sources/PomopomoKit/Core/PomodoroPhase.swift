3a994bebceeb0cf43a603fd582e0b5dc38ca61e6
Amarnath Prasad <amrnth007@gmail.com>
2026-07-05 10:03:55 +0530

    update display title

--- Sources/PomopomoKit/Core/PomodoroPhase.swift
+++ Sources/PomopomoKit/Core/PomodoroPhase.swift
@@ -6,7 +6,7 @@ public enum PomodoroPhase: String, Codable, Sendable, Equatable {
 
     public var displayTitle: String {
         switch self {
-        case .pomoPomo: "Pomodoro"
+        case .pomoPomo: "PomoPomo"
         case .break: "Break"
         }
     }
